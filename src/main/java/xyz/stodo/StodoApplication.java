e691bec22c0a625e8c22cbc51bfddfe1884aa436
kostyan87 <kostya.ignatev.14@mail.ru>
2023-04-06 12:30:27 +0300

    Создана базовая структура проекта

---
+++ src/main/java/xyz/stodo/StodoApplication.java
@@ -0,0 +1,13 @@
+package xyz.stodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/stodo/StodoApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.stodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
